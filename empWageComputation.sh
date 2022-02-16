ead341486b0df8815b0a5d525235968a8e0629be
pappurathod <prathod4545@gmail.com>
2022-02-16 09:20:44 +0530

    check emp part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,7 +3,6 @@ echo "Welcome to Employee Wage Computation program"
 ratePrHr=20
 isPresent=1
 check=$((RANDOM%2))
-
 if [ $check -eq $isPresent ]
 then
 	echo "Employee is present"
@@ -12,5 +11,18 @@ else
 	echo "Employee is absent"
 	wrKHr=0
 fi
-salary=$(($ratePrHr*wrKHr))
+salary=$(($ratePrHr*$wrKHr))
 echo "Daily salary is $salary"
+
+
+partTm=$((RANDOM%2))
+if [ $partTm -eq $isPresent ]
+then
+	echo "Emp is present"
+	partWrk=8
+else
+	echo "Emp is absent"
+	partWrk=0
+fi
+partTmSalary=$(($ratePrHr*$partWrk))
+echo "Par time salary is $partTmSalary"
